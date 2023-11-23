1b4ec1c94b52aa96db8324aa5dfee52937e5df42
FelipeAssisL <91689659+FelipeAssisL@users.noreply.github.com>
2023-11-23 19:57:20 -0300

    Add Tutor class
    Adicionado classe tutor

---
+++ src/edu/curso/Tutor.java
@@ -0,0 +1,35 @@
+package edu.curso;
+
+public class Tutor {
+	
+	private String nomeTutor;
+	private String rgTutor;
+	private String especialidadeTutor;
+	private String descricaoTutor;
+	
+	public String getNomeTutor() {
+		return nomeTutor;
+	}
+	public void setNomeTutor(String nomeTutor) {
+		this.nomeTutor = nomeTutor;
+	}
+	public String getRgTutor() {
+		return rgTutor;
+	}
+	public void setRgTutor(String rgTutor) {
+		this.rgTutor = rgTutor;
+	}
+	public String getEspecialidadeTutor() {
+		return especialidadeTutor;
+	}
+	public void setEspecialidadeTutor(String especialidadeTutor) {
+		this.especialidadeTutor = especialidadeTutor;
+	}
+	public String getDescricaoTutor() {
+		return descricaoTutor;
+	}
+	public void setDescricaoTutor(String descricaoTutor) {
+		this.descricaoTutor = descricaoTutor;
+	}
+
+}
