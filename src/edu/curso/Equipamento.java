566cd200b5594a32acb1f38a6f3c5849d913a936
FelipeAssisL <91689659+FelipeAssisL@users.noreply.github.com>
2023-11-30 17:35:28 -0300

    Criação da classe Equipamento

---
+++ src/edu/curso/Equipamento.java
@@ -0,0 +1,26 @@
+package edu.curso;
+
+public class Equipamento {
+	private int idEquip;
+	private String nomeEquipe;
+	private String descricaoEquip;
+	
+	public int getIdEquip() {
+		return idEquip;
+	}
+	public void setIdEquip(int idEquip) {
+		this.idEquip = idEquip;
+	}
+	public String getNomeEquipe() {
+		return nomeEquipe;
+	}
+	public void setNomeEquipe(String nomeEquipe) {
+		this.nomeEquipe = nomeEquipe;
+	}
+	public String getDescricaoEquip() {
+		return descricaoEquip;
+	}
+	public void setDescricaoEquip(String descricaoEquip) {
+		this.descricaoEquip = descricaoEquip;
+	}
+}
